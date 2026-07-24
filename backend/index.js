9f9cb0026298a8b811784f24837f9b6aecd57b8c
My-project <kumarankit@gmail.com>
2026-07-24 11:10:47 +0530

    Update CORS configuration to allow Vercel frontend

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const port = process.env.PORT || 9000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://brijwalla-lovat.vercel.app"],
     credentials: true,
   })
 );
